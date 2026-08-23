d0bd7f1ce6561e53944e9bb348867655688d546e
toshiki-s1892 <toshiki.suo1892@gmail.com>
2026-08-23 23:08:42 +0900

    chore: categories機能に向けて依存パッケージ・Lint/tsconfig設定を更新

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,10 @@
 import { nextJsConfig } from "@repo/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default [
+  ...nextJsConfig,
+  {
+    // msw init が生成するファイル。手動編集・lint対象外
+    ignores: ["public/mockServiceWorker.js"],
+  },
+];
